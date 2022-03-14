ddfee60a7a8c095866d743c80699707cc9baf4de
Yan Oliveira <oliveira.yan02@gmail.com>
2022-03-14 15:44:33 -0300

    refactor: remove root import

--- src/main/App/index.spec.tsx
+++ src/main/App/index.spec.tsx
@@ -1,6 +1,6 @@
 import 'react-native';
 import React from 'react';
-import App from '~/main/App';
+import App from '../App';
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
