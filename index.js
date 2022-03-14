64aff16cc35d44ba28df6852be58f5d92638dc15
Yan Oliveira <oliveira.yan02@gmail.com>
2022-03-14 15:46:52 -0300

    refactor: normalize app folder name

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { AppRegistry } from 'react-native';
-import App from './src/main/App';
+import App from './src/main/app';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
--- src/main/app/index.spec.tsx
+++ src/main/app/index.spec.tsx
@@ -1,6 +1,6 @@
 import 'react-native';
 import React from 'react';
-import App from '../App';
+import App from '../app';
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
