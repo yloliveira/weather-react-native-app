8e5012ab77fd7d10b48fc34f5e1a1f3c54c53474
Yan Oliveira <oliveira.yan02@gmail.com>
2022-03-14 11:54:23 -0300

    chore: init project with rn-cli using typescript template

---
+++ src/main/App/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => {
+  return (
+
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: "center",
+      }}>
+      <Text>Weather App</Text>
+    </View>
+  );
+};
+
+export default App;
