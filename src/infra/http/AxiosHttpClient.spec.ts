f49e3c74a53b1a1ba26424b3201d534a833bee60
Yan Oliveira <oliveira.yan02@gmail.com>
2022-03-15 16:11:54 -0300

    test[AxiosHttpClient]: should call axios with correct values

---
+++ src/infra/http/AxiosHttpClient.spec.ts
@@ -0,0 +1,65 @@
+import axios from 'axios';
+import {
+  IHttpClient,
+  HttpRequest,
+  HttpMethod,
+} from '../../data/protocols/http/IHttpClient';
+import { AxiosHttpClient } from './AxiosHttpClient';
+
+jest.mock('axios');
+
+export const mockHttpRequest = (): HttpRequest => ({
+  url: 'valid_url',
+  method: HttpMethod.get,
+  headers: {
+    authorization: 'valid_token',
+  },
+  params: {
+    param1: 'valid_param1',
+    param2: 'valid_param2',
+  },
+  body: {
+    field1: 'valid_field1',
+    field2: 'valid_field2',
+  },
+});
+
+const mockAxios = (): jest.Mocked<typeof axios> => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
+  mockedAxios.request.mockClear().mockResolvedValue({
+    status: 200,
+    data: {},
+  });
+  return mockedAxios;
+};
+
+type SutTypes = {
+  sut: IHttpClient;
+  mockedAxios: jest.Mocked<typeof axios>;
+};
+
+const makeSut = (): SutTypes => {
+  const sut = new AxiosHttpClient();
+  const mockedAxios = mockAxios();
+  return {
+    sut,
+    mockedAxios,
+  };
+};
+
+describe('AxiosHttpClient', () => {
+  it('Should call axios with correct values', async () => {
+    const { sut, mockedAxios } = makeSut();
+    const request = mockHttpRequest();
+
+    await sut.request(request);
+
+    expect(mockedAxios.request).toHaveBeenCalledWith({
+      url: request.url,
+      method: request.method,
+      headers: request.headers,
+      params: request.params,
+      data: request.body,
+    });
+  });
+});
