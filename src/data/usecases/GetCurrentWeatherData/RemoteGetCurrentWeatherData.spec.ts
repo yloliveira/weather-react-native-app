28c83211967ca27324f88a50d2e146b0126e3c81
Yan Oliveira <oliveira.yan02@gmail.com>
2022-03-15 21:09:55 -0300

    test[RemoteGetCurrentWeatherDataUseCase]: should throw UnexpectedError if HttpClient returns error

--- src/data/usecases/GetCurrentWeatherData/RemoteGetCurrentWeatherData.spec.ts
+++ src/data/usecases/GetCurrentWeatherData/RemoteGetCurrentWeatherData.spec.ts
@@ -1,7 +1,11 @@
 import { RemoteGetCurrentWeatherData } from './RemoteGetCurrentWeatherData';
 import { IGetCurrentWeatherData } from '../../../domain/usecases/IGetCurrentWeatherData';
 import { MockHttpClient } from '../../../data/mocks/MockHttpClient';
-import { HttpMethod } from '../../../data/protocols/http/IHttpClient';
+import {
+  HttpMethod,
+  HttpStatusCode,
+} from '../../../data/protocols/http/IHttpClient';
+import { UnexpectedError } from '../../../domain/errors/UnexpectedError';
 
 type SutTypes = {
   sut: IGetCurrentWeatherData;
@@ -29,5 +33,22 @@ describe('RemoteGetCurrentWeatherDataUseCase', () => {
     expect(httpClientMock.url).toBe(url);
     expect(httpClientMock.method).toBe(HttpMethod.get);
     expect(httpClientMock.params).toEqual(params);
+  });
+
+  test('Should throw UnexpectedError if HttpClient returns error', async () => {
+    const url = 'valid_url';
+    const { sut, httpClientMock } = makeSut(url);
+    const params = {
+      latitude: -21.757793,
+      longitude: -43.35372,
+    };
+
+    httpClientMock.response = {
+      statusCode: HttpStatusCode.serverError,
+    };
+
+    const promise = sut.execute(params);
+
+    await expect(promise).rejects.toThrow(new UnexpectedError());
   });
 });
