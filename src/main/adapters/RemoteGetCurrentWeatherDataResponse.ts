e030d8b3eedc7dffab821f4d124a18a5c289e945
Yan Oliveira <oliveira.yan02@gmail.com>
2022-03-16 16:12:32 -0300

    refactor: data format improvements

--- src/main/adapters/RemoteGetCurrentWeatherDataResponse.ts
+++ src/main/adapters/RemoteGetCurrentWeatherDataResponse.ts
@@ -19,55 +19,84 @@ export class RemoteGetCurrentWeatherDataResponseAdapter
   }
 
   private buildResponseResult(data: any) {
+    const city = data.name;
+    const temperature = `${data.main.temp.toFixed(0)}º`;
+    const weather = data.weather[0].description;
+    const tempMin = `${data.main.temp_min.toFixed(0)}º`;
+    const tempMax = `${data.main.temp_max.toFixed(0)}º`;
+    const feelsLike = `${data.main.feels_like.toFixed(0)}º`;
+    const humidity = `${data.main.humidity}%`;
+    const visibility = `${data.visibility / 1000}km`;
+    const pressure = `${data.main.pressure}\n hPa`;
+    const windSpeed = `${data.wind.speed.toFixed(1)}\n km/h`;
+
+    const sunriseDate = new Date(data.sys.sunrise * 1000);
+    const sunrise = `${sunriseDate
+      .getHours()
+      .toString()
+      .padStart(2, '0')}:${sunriseDate
+      .getMinutes()
+      .toString()
+      .padStart(2, '0')}`;
+
+    const sunsetDate = new Date(data.sys.sunset * 1000);
+    const sunset = `${sunsetDate
+      .getHours()
+      .toString()
+      .padStart(2, '0')}:${sunsetDate
+      .getMinutes()
+      .toString()
+      .padStart(2, '0')}`;
+
     return {
-      city: data.name,
-      temperature: data.main.temp.toFixed(0),
-      weather: data.weather[0].description,
+      city,
+      temperature,
+      weather,
       data: [
         {
           key: 'sunrise',
           title: 'Nascer do sol',
-          value: data.sys.sunrise,
+          value: sunrise,
         },
         {
           key: 'sunset',
           title: 'Pôr do sol',
-          value: data.sys.sunset,
+          value: sunset,
         },
         {
           key: 'temp_min',
           title: 'Temperatura Mínima',
-          value: data.main.temp_min,
+          value: tempMin,
         },
         {
           key: 'temp_max',
           title: 'Temperatura Máxima',
-          value: data.main.temp_max,
+          value: tempMax,
         },
         {
           key: 'feels_like',
           title: 'Sensação térmica',
-          value: data.main.feels_like,
+          value: feelsLike,
         },
         {
           key: 'humidity',
           title: 'Umidade',
-          value: data.main.humidity,
+          value: humidity,
         },
         {
           key: 'visibility',
           title: 'Visibilidade',
-          value: data.visibility,
+          value: visibility,
         },
         {
           key: 'pressure',
           title: 'Pressão',
-          value: data.main.pressure,
+          value: pressure,
         },
         {
           key: 'wind_speed',
           title: 'Veliocidade do vento',
-          value: data.wind.speed,
+          value: windSpeed,
         },
       ],
     };
